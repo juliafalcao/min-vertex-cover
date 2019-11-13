b9a69885b3482fcaf9af84b79a973e02d9050529
Júlia Falcão <juliafsfalcao@hotmail.com>
2019-11-13 17:32:24 -0300

    update results.py

--- results.py
+++ results.py
@@ -1,41 +1,20 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 # df: pd.DataFrame = pd.read_csv("results/base.csv")
-df: pd.DataFrame = pd.read_csv("results/base.csv")
+dfb: pd.DataFrame = pd.read_csv("results/base.csv")
 
-df["valid"] = df["opt"] < df["N"]
-df = df[df["valid"]] # remove invalid instances
-df = df.drop(columns=["valid"])
-
-"""
-grasp alpha comparison
-"""
-
-# df_alpha = pd.read_csv("results/grasp-tests.csv")
-# df_alpha = df_alpha.sort_values(by=["instance", "mvc_size"], ascending=[True, True])
-# df_alpha = df_alpha.drop_duplicates(subset=["instance"], keep="first")
-
-
-# labels, counts = np.unique(df_alpha["alpha"], return_counts=True)
-# labels = ["α = " + str(label) for label in labels]
-# # counts = [count/25 for count in counts]
-
-# fig, ax = plt.subplots(1, 1)
-# ax.bar(labels, counts, align='center', width=0.5, color=[cm.viridis(i/5) for i in range(5)][1:4])
-# ax.grid(True, axis="y", zorder=1000)
-# ax.set_axisbelow(True)
-# ax.set_ylabel("Nº de melhores resultados")
-# ax.set_xlabel("Valores de alfa")
-# plt.show()
+dfb["valid"] = dfb["opt"] < dfb["N"]
+dfb = dfb[dfb["valid"]] # remove invalid instances
+dfb = dfb.drop(columns=["valid"])
 
 """
 GRASP vs. GRASP-PR comparison
 """
 df_g = pd.read_csv("results/grasp.csv")
-df_pr = pd.read_csv("results/grasp-pr-prelim.csv") # [!]
+df_pr = pd.read_csv("results/grasp-pr.csv") # [!]
 
 # organize dfs
 df_g = df_g[df_g["alpha"] == 0.3]
@@ -49,9 +28,17 @@ df_pr = df_pr.sort_values(by=["instance", "grasp-pr"], ascending=[True, True])
 df_pr = df_pr.drop_duplicates(subset=["instance"], keep="first") # keep only the best result per instance
 
 df = df_g.merge(df_pr, how="inner", on="instance")
-df = df.drop(labels=["alpha"], axis=1)
+# df = df.drop(labels=["alpha"], axis=1)
 
 df["result"] = df.apply(lambda row: "better" if (row["grasp-pr"] < row["grasp"]) else "equal" if (row["grasp-pr"] == row["grasp"]) else "worse", axis=1)
-df.loc[df[df["grasp-pr"] > df["grasp"]], "result"] = "better"
 
-df.to_csv("results/formatted_grasp.csv")
+df = df.merge(dfb[["instance", "opt", "N"]], how="inner", on="instance")
+df = df[["instance", "N", "opt", "grasp", "t(grasp)", "grasp-pr", "t(grasp-pr)", "result"]]
+df["t(grasp)"] = df["t(grasp)"].apply(lambda x: x*0.001)
+df["t(grasp-pr)"] = df["t(grasp-pr)"].apply(lambda x: x*0.001)
+
+df.to_csv("results/formatted_grasp.csv", index=False)
+
+# line plot
+df["grasp"] = df["opt"]/df["grasp"]
+df["grasp"] = df["opt"]/df["grasp-pr"]
